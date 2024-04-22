1cf66332186b72b6289c22498742728866112b28
mathpvx <7547@holbertonstudents.com>
2024-04-22 15:18:55 +0200

    put doctring before import

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-import math
 """type-annotated function floor which takes
 a float n as argument and returns the floor of the float"""
+import math
 
 
 def floor(n: float) -> int:
