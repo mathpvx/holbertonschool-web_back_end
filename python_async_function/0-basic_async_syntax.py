6fe3eb1aabc2cc4c5e8d6a1f918070054efb808e
mathpvx <7547@holbertonstudents.com>
2024-04-23 15:02:08 +0200

    added space

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int =10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """  takes in an integer argument (max_delay, with a default value of 10)
     named wait_random that waits for a random delay between 0 and max_delay
     (included and float value) seconds and eventually returns it. """
