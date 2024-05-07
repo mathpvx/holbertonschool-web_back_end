0e742441869d84bab935ee2300a3d9e04a7b87fa
mathpvx <7547@holbertonstudents.com>
2024-05-07 10:48:03 +0200

    added comments and indent

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,9 +1,10 @@
 export default function taskBlock(trueOrFalse) {
 	const task = false;
 	const task2 = true;
-
 	if (trueOrFalse) {
+		// eslint-disable-next-line
 		const task = true;
+		// eslint-disable-next-line
 		const task2 = false;
 	}
 
