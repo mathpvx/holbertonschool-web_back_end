dd2f62b6517b0da3fdd5ec258de1e111419d869c
mathpvx <7547@holbertonstudents.com>
2024-05-17 14:28:34 +0200

    semistandard

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+export default function handleProfileSignup () {
   const promises = [uploadPhoto(), createUser()];
   return Promise.all(promises)
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch (() => {
+    .catch(() => {
       console.log('Signup system offline');
     });
 }
