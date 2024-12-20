680f6603a39681a72a857bb29bbd3fef080d8b31
mathpvx <7547@holbertonstudents.com>
2024-12-20 09:24:31 +0100

    task 3

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" LRU Caching """
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ class LRUCache that inherits
+    from BaseCaching and is a caching system """
+
+    def __init__(self):
+        """ Initialize the class """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                # Remove the existing item so it can be added to the end
+                self.cache_data.pop(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Pop the first item in FIFO order (least recently used)
+                discarded_key, _ = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {discarded_key}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is not None and key in self.cache_data:
+            # Move the accessed item to the end to mark it as recently used
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
+        return None
