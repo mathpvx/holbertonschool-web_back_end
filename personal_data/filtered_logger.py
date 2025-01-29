b0f2a3d4f522f3c1a7510570d5ccb29138a99945
mathpvx <7547@holbertonstudents.com>
2025-01-29 10:20:38 +0100

    changed pattern and added docstring

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """
 Function that returns the log message obfuscated
 """
+
 import re
 from typing import List
 
@@ -12,11 +13,12 @@ def filter_datum(
     message: str,
     separator: str
 ) -> str:
+    """Function that returns the log message obfuscated"""
 
     for field in fields:
         # defines pattern as 'anything but special characters in field' =
         # 'anything but the separator'
-        pattern = fr"{re.escape(field)}=([^({separator})]+)"
+        pattern = fr"{re.escape(field)}=[^{re.escape(separator)}]*"
         message = re.sub(pattern, f"{field}={redaction}", message)
 
     return message
