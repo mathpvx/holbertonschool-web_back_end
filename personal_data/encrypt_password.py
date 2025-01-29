b46d9006614cf12129f08fb7072d6f1d6bb6d1ec
mathpvx <7547@holbertonstudents.com>
2025-01-29 13:19:21 +0100

    implemented a is valid function that returs boolean

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -12,3 +12,10 @@ def hash_password(password: str) -> bytes:
     """
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if a given password matches the stored hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
