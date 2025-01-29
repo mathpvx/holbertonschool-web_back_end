5f75544bf25fb134a2d041fbc5a614aa8bd6e5f0
mathpvx <7547@holbertonstudents.com>
2025-01-29 11:32:24 +0100

    update class to accept a list of str

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ Function that returns the log message obfuscated
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -22,3 +23,21 @@ def filter_datum(
         message = re.sub(pattern, f"{field}={redaction}", message)
 
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ custom class that inherits from logging lib
+    that defines how log messages should be formatted
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
