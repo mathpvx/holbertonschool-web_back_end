5dde5269570f4c5d612b1003afa1650b51e832e7
mathpvx <7547@holbertonstudents.com>
2025-02-07 16:40:55 +0100

    add unauthorized route

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-""" Module of Index views
-"""
+""" Module of Index views """
 from flask import jsonify, abort
 from api.v1.views import app_views
-
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
@@ -12,7 +10,6 @@ def status() -> str:
       - the status of the API
     """
     return jsonify({"status": "OK"})
-
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -24,3 +21,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    This route triggers a 401 Unauthorized error
+    """
+    abort(401)
