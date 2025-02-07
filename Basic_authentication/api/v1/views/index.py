473efda682b50ecab40bcad8c9a4ba68f1fde269
mathpvx <7547@holbertonstudents.com>
2025-02-07 16:45:20 +0100

    pycodestyle

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -3,6 +3,7 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -10,6 +11,7 @@ def status() -> str:
       - the status of the API
     """
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -21,6 +23,7 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
