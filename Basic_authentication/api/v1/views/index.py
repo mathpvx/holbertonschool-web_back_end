6354810dc2b663ee96e6e698a788430b0a2301e3
mathpvx <7547@holbertonstudents.com>
2025-02-07 16:48:34 +0100

    add forbidden route

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -3,7 +3,6 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -11,7 +10,6 @@ def status() -> str:
       - the status of the API
     """
     return jsonify({"status": "OK"})
-
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -24,10 +22,16 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     This route triggers a 401 Unauthorized error
     """
     abort(401)
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    This route triggers a 403 Forbidden error
+    """
+    abort(403)
