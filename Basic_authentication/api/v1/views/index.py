ba1dc934e6e2d079ecddac1ad05d311d3ed108aa
mathpvx <7547@holbertonstudents.com>
2025-02-07 16:52:53 +0100

    pycodestyle

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -3,6 +3,7 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
