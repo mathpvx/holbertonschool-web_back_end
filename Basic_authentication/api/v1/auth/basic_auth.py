f60533d62df946d5de5b38cb1d61d65c4e61bd83
mathpvx <7547@holbertonstudents.com>
2025-02-14 13:26:53 +0100

    base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Basic authentication module """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -13,15 +14,38 @@ class BasicAuth(Auth):
         Extracts the Base64 part of the Authorization header.
 
         Returns:
-        - None if authorization_header is None or not a string.
-        - None if authorization_header doesn't start with "Basic ".
-        - The Base64 part of the Authorization header otherwise.
+            - None if authorization_header is None or not a string.
+            - None if authorization_header doesn't start with "Basic ".
+            - The Base64 part of the Authorization header otherwise.
         """
         if authorization_header is None or not isinstance(
-                authorization_header, str):
+            authorization_header, str
+        ):
             return None
 
         if not authorization_header.startswith("Basic "):
             return None
 
         return authorization_header[6:]  # Extract everything after "Basic "
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """
+        Decodes a Base64 string.
+
+        Returns:
+            - None if base64_authorization_header is None or not a string.
+            - None if base64_authorization_header is not a valid Base64 string.
+            - The decoded UTF-8 string otherwise.
+        """
+        if base64_authorization_header is None or not isinstance(
+            base64_authorization_header, str
+        ):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode("utf-8")
+        except Exception:
+            return None
