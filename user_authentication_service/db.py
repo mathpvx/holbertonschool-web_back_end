f6a62e25c0be60cf4962153adc7fcd2410afc837
mathpvx <7547@holbertonstudents.com>
2025-03-07 13:07:06 +0100

    method that finds the first match in User table with whatever argument in input

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -3,6 +3,7 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
@@ -38,3 +39,15 @@ class DB:
         self._session.refresh(new_user)
 
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """method that finds the first match in User table with whatever argument in input"""
+        query = self._session.query(User).filter_by(**kwargs).first()
+
+        if query is None:
+            raise NoResultFound()
+
+        if not kwargs:
+            raise InvalidRequestError
+
+        return query
