0a4191834eafab9e72e4d54bcb8194fba6b640d7
mathpvx <7547@holbertonstudents.com>
2025-03-14 10:10:56 +0100

    create new user or raise err if already exists

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python3
 """ authentification methods
 """
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
 import bcrypt
 
 
-def _hash_password(password: str) -> bytes:
-    """ method that takes in a password string arg and returns bytes"""
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+class Auth:
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """ method that takes in a password string arg and returns bytes"""
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
+
+    def register_user(self, email: str, password: str) -> User:
+        """method that checks if the user already exists
+        and creates one if not the case"""
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            if existing_user:
+                raise ValueError(f"User {email} already exists")
+
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
