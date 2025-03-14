da412b2335bc24529f4749d66bcb2038a9beb1b8
mathpvx <7547@holbertonstudents.com>
2025-03-14 11:26:55 +0100

    end-point to register a user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
-""" Basic Flask App"""
-from flask import Flask, jsonify
+""" Flask app with user registration endpoint """
+from flask import Flask, request, jsonify
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route("/", methods=["GET"])
-def home():
-    """ handles GET requests"""
-    return jsonify({"message": "Bienvenue"})
+@app.route("/users", methods=["POST"])
+def users():
+    """
+    registers a new user if the email is not already registered
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        return jsonify({"message": "Missing email or password"}), 400
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
