467d7eab7e7eb34d80e8af0e385de2abf9fd3ca7
mathpvx <7547@holbertonstudents.com>
2025-03-14 11:42:00 +0100

    get user from session id

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -56,3 +56,13 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """returns the user corresponding to the given session id
+        or none if session id is none or no user is found"""
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
