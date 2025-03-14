484d6b996866155006232db4fd5a96d13eb8ef87
mathpvx <7547@holbertonstudents.com>
2025-03-14 11:30:15 +0100

    valid login method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-""" authentification methods
-"""
+""" authentification methods """
+
 from db import DB
 from user import User
 from sqlalchemy.exc import NoResultFound
@@ -8,25 +8,36 @@ import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
-    """ method that takes in a password string arg and returns bytes"""
+    """method that takes in a password string arg and returns bytes"""
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+    return bcrypt.hashpw(password.encode("utf-8"), salt)
 
 
 class Auth:
+    """auth class to interact with the authentication database"""
+
     def __init__(self):
+        """initialize auth instance"""
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """method that checks if the user already exists
-        and creates one if not the case"""
+        """method that checks if the
+        user already exists and creates one if not the case"""
         try:
             existing_user = self._db.find_user_by(email=email)
             if existing_user:
                 raise ValueError("User {} already exists".format(email))
-
         except NoResultFound:
-            hashed_password = _hash_password(password)
-            new_user = self._db.add_user(email, hashed_password)
-            return new_user
+            hashed_password = _hash_password(password).decode("utf-8")
+            return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """check if the email and password provided match a registered user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode("utf-8"),
+                user.hashed_password.encode("utf-8")
+            )
+        except NoResultFound:
+            return False
