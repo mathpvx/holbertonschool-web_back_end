6c0a0aa2d6ea2ad903fcb9b72f872d57a846aaaf
mathpvx <7547@holbertonstudents.com>
2025-03-14 11:50:37 +0100

    generate reset password token

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -73,3 +73,14 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """finds the user by email, generates a password reset token,
+        updates the user reset_token in the database and returns the token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError("User not found")
