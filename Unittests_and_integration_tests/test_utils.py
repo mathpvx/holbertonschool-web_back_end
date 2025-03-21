fdc0e7977a688dde5ac317aefa919c3a848ca4aa
mathpvx <7547@holbertonstudents.com>
2025-03-21 11:25:32 +0100

    test passes

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -11,11 +11,11 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
-        ("case1", {'a': 1}, ('a',), 1),
-        ("case2", {'a': {'b': 2}}, ('a',), {'b': 2}),
-        ("case3", {'a': {'b': 2}}, ('a', 'b'), 2),
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2),
     ])
-    def test_access_nested_map(self, _, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(
             access_nested_map(nested_map, path), expected
         )
