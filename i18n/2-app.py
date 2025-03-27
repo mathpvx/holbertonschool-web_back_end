b351fdf5a1591b4b368ef0a88919fd31cf26e3fe
mathpvx <7547@holbertonstudents.com>
2025-03-27 11:08:55 +0100

    function to match with the supported language

---
+++ i18n/2-app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Basic Flask App
+
+This module creates a simple Flask web application
+with one route that renders a basic HTML template.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Config class for Flask app and Babel.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel()
+babel.init_app(app, locale_selector=lambda: get_locale())
+
+
+def get_locale():
+    """
+    Select the best matching language based on the request.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello():
+    """
+    Route handler for the root URL.
+
+    Returns:
+        str: Rendered HTML content from the template.
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
