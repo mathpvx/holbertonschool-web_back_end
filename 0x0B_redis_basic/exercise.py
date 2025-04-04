02d3314821762a068e5f5182577b94bd4029cc04
mathpvx <7547@holbertonstudents.com>
2025-04-04 11:17:07 +0200

    decorator count

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 """
-cache module using redis to store data
+cache module using redis to store data and track usage
 """
 
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """count how many times a method is called"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -16,6 +27,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store data in redis under a random key"""
         key = str(uuid.uuid4())
