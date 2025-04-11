31335e9cb7e41b10a18f051736eb15fa05bb6069
mathpvx <7547@holbertonstudents.com>
2025-04-11 11:37:38 +0200

    basic mySQL table

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- creates 'users' table with id email and name
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
