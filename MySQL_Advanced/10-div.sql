71b37f258b8a7b529288e9c97e94250a2e852325
mathpvx <7547@holbertonstudents.com>
2025-04-18 11:14:46 +0200

    safe divide

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- Returns a / b or 0 if b = 0
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END$$
+
+DELIMITER ;
