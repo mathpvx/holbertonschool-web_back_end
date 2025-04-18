c17c158ddfbb85ca4d739322d7169871aab30716
mathpvx <7547@holbertonstudents.com>
2025-04-18 10:55:03 +0200

    compute and update users av score

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,18 @@
+-- procedure to compute and update a user's average score
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE corrections.user_id = user_id;
+
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+END$$
+
+DELIMITER ;
