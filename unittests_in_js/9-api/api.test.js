b8484485b89cbb8b8f7960d875be7b60070189fc
mathpvx <7547@holbertonstudents.com>
2025-05-19 10:47:57 +0200

    api test

---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,35 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  it('should return status code 200', (done) => {
+    request.get('http://localhost:7865/', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return correct body', (done) => {
+    request.get('http://localhost:7865/', (err, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('should return 200 when id is a number', (done) => {
+    request.get('http://localhost:7865/cart/12', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return 404 when id is NOT a number', (done) => {
+    request.get('http://localhost:7865/cart/hello', (err, res, body) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
