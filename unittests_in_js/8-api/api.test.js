001824faa00ba312268d0357dd63717215411fec
mathpvx <7547@holbertonstudents.com>
2025-05-19 10:44:43 +0200

    api test

---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,20 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  const url = 'http://localhost:7865/';
+
+  it('should return status code 200', (done) => {
+    request.get(url, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return correct response body', (done) => {
+    request.get(url, (err, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
