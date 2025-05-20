3fa502d46b565e410b4f227219a4e90b8d234829
mathpvx <7547@holbertonstudents.com>
2025-05-20 14:25:45 +0200

    created a job with kue

---
+++ queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,25 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'This is the code to verify your account',
+};
+
+const job = queue.create('push_notification_code', jobData);
+
+job
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
