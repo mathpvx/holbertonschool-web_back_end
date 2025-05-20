07e9fd5e0a278ae0d89cc08e19e171a594e36e0c
mathpvx <7547@holbertonstudents.com>
2025-05-20 10:21:13 +0200

    using async await instead of cb

---
+++ queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,33 @@
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+const getAsync = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(value);
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+(async () => {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+})();
