d83585757e1715c8ec2781662f80bd9af5e9b845
mathpvx <7547@holbertonstudents.com>
2025-05-20 10:15:24 +0200

    2 func to set a key and display key value

---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,29 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.error('Error:', err);
+      return;
+    }
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
